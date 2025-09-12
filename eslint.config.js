1f36d5f3768cc4845ee8ed6a1282c1a768eac0ea
hebertcisco <hebertcisco@outlook.com>
2025-09-12 15:38:34 -0300

    v0.2.7

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+// ESLint v9 flat config
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const jestPlugin = require('eslint-plugin-jest');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  // Ignored paths (replaces .eslintignore)
+  {
+    ignores: ['node_modules', 'lib', 'coverage'],
+  },
+
+  // Bring in recommended rule sets via compat
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended'
+  ),
+
+  // Project-specific settings
+  {
+    files: ['**/*.{ts,js}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      jest: jestPlugin,
+    },
+    rules: {
+      // Add project-specific overrides here if needed
+    },
+  },
+];
